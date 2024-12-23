efdd7a26964d0589d569705d6527531b1b3e8846
Aneesh165 <aneeshmp165@gmail.com>
2024-12-23 11:23:22 +0530

    Sign In Api

--- Api/Routes/AuthRoute.js
+++ Api/Routes/AuthRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { signup } from '../Controllers/AuthController.js';
+import { signin, signup } from '../Controllers/AuthController.js';
 
 
 const router = express.Router();
 
 router.post("/signup", signup)
+router.post("/signin", signin)
 
 export default router
