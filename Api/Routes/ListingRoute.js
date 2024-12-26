b4f7ec636784d893b82146cd13a18c57ddc6e0ad
Aneesh165 <aneeshmp165@gmail.com>
2024-12-26 12:01:04 +0530

    listingroute

--- Api/Routes/ListingRoute.js
+++ Api/Routes/ListingRoute.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { createListing } from '../Controllers/ListingController.js';
+import { createListing, deleteListing, getListing, getListings, updateListing } from '../Controllers/ListingController.js';
 import { verifyToken } from '../Utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
+router.delete('/delete/:id', verifyToken, deleteListing);
+router.post('/update/:id', verifyToken, updateListing);
+router.get('/get/:id', getListing);
+router.get('/get', getListings);
 
 export default router;
