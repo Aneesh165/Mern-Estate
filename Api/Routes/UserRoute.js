cf3398a867f315bdd206d144a1dff24f569eade1
Aneesh165 <aneeshmp165@gmail.com>
2024-12-26 12:01:20 +0530

    userroute

--- Api/Routes/UserRoute.js
+++ Api/Routes/UserRoute.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import { deleteUser, updateUser } from '../Controllers/UserController.js';
+import { deleteUser, updateUser,  getUserListings, getUser } from '../Controllers/UserController.js';
 import { verifyToken } from '../Utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/update/:id', verifyToken, updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
+router.get('/listings/:id', verifyToken, getUserListings)
+router.get('/:id', verifyToken, getUser)
+
 
 export default router
